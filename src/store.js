482cf514a0ca9449c97daf4b956bbfa6a5988ae4
thorington26 <latrisha.ct@gmail.com>
2026-01-02 06:29:50 -0500

    commit changes

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
 
 export default configureStore({
   reducer: {
